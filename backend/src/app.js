6a97ca577721ab2bd0b7b7862d6c18cafdceb4f1
Silviana <silvistudies@gmail.com>
2020-08-04 11:56:42 +0800

    lint doublequote

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,20 @@
-import logger from 'morgan';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import createError from 'http-errors';
+import logger from "morgan";
+import express from "express";
+import cookieParser from "cookie-parser";
+import createError from "http-errors";
 // import expressValidator from 'express-validator';
-import cors from 'cors';
-import indexRouter from './routes/index';
+import cors from "cors";
+import indexRouter from "./routes/index";
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(expressValidator());
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 // error handler
 app.use((error, req, res, next) => {
   // eslint-disable-next-line no-console
-  console.log('meow, this is an error');
+  console.log("meow, this is an error");
   res.status(error.status || 500).json({
     error: error.message,
     code: error.status,
